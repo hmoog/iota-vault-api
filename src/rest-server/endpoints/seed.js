893e4b1bb9c972aa5649276a20b1981269c01154
Hans Moog <hm@mkjc.net>
2018-03-06 21:23:42 +0100

    files_changed

--- src/rest-server/endpoints/seed.js
+++ src/rest-server/endpoints/seed.js
@@ -51,7 +51,7 @@ var create = function create(req, res, next) {
                 errorCorrectionLevel: 'H',
                 scale: 3,
                 color: {
-                    dark: '#FFFFFFFF',  // Blue dots
+                    dark: '#000000FF',  // Blue dots
                     light: '#0000' // Transparent background
                 }
             }).then((qrcode) => {
